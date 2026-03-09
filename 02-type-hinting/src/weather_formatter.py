d0e66ac8b3b13391ea2a8265246d9a1cfc0dcf87
skv.analyst <skv.analyst@gmail.com>
2026-03-09 15:42:01 +0300

    add: модуль форматирования итогового результата

---
+++ 02-type-hinting/src/weather_formatter.py
@@ -0,0 +1,5 @@
+from weather_api_service import Weather
+
+def format_weather(weather: Weather) -> str:
+    """Форматирует итоговый вывод погоды"""
+    return "Здесь будет какой-то формат данных из Weather"
